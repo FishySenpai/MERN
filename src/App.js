d90b23eb0a348f38d26cffc033bb03fe85a7ad52
FishySenpai <akanomanbasit@gmail.com>
2023-03-14 22:58:43 +0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import Login from './components/User/Login';
+import Registration from './components/User/Registration';
 import './App.css';
-
+import { Router, Routes,Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/login" element={Login}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Login from "./User/Login";
+import Registration from "./User/Registration";
+export default{
+    Login,
+    Registration,
+}
